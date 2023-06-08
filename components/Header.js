082daa47b81bfb0772484408724fd92a375b3cc8
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-08 12:38:42 +0530

    5 files modified

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,16 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <div className="w-full flex">
+    <div className="w-full">
       <Navbar />
-      <div className="flex justify-content-space-between">
+      {/* <div className="flex justify-content-space-between items-center">
         <div className="p-2">manjunathkalburgi@gmail.com</div>
-        <div className="p-2 flex flex-wrap ">
+        <div className="p-2 flex flex-wrap" style={{ minWidth: 70 }}>
           <Avatar />
         </div>
         <div className="p-2">Messages</div>
         <div className="p-2">Notifications</div>
-      </div>
+      </div> */}
     </div>
   );
 }
